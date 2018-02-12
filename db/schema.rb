faf0b02784bb51f6cfa336c7649fa990d161554e
Deyan Denchev <deyanskiq@abv.bg>
2018-02-13 01:41:30 +0200

    fix comfirmable and remove locable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208115248) do
+ActiveRecord::Schema.define(version: 20180212232228) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "body"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20180208115248) do
     t.string "name"
     t.bigint "reseller_id"
     t.string "role"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
