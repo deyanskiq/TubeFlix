c2e886502c5716e294f4790c3f7c4400cb41ef7e
Deyan Denchev <deyanskiq@abv.bg>
2018-02-27 20:25:58 +0200

    add positive and negative specs for validation for Upload and Comment models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,15 +6,24 @@ describe 'Comment' do
   context 'test' do
 
     before(:all) {User.all.map(&:destroy)}
+    before(:all) {Upload.all.map(&:destroy)}
 
+    # binding.pry
     let(:user) {User.create(id: 1, name: 'test', email: 'test@abv.bg', password: '123123', role: 'Reseller')}
-    let(:upload) {Upload.create(name: 'upload', user_id: 1)}
+    let(:upload) {Upload.create(id: 1, name: 'upload', user_id: 1, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/mp4", video_file_size: 1055736)}
 
 
-    it 'ff' do
+    it 'validates successfully' do
 
-      Comment.create(body: "test")
-      raise unless 1 == 1
+      a = Comment.new(upload_id: 1, user_id: 1, body: "test")
+      expect(a.save).to be true
+
+    end
+
+    it 'negative test when body do not presence' do
+
+      a = Comment.new(id: 1)
+      expect(a.save).to be false
 
     end
   end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -8,13 +8,56 @@ describe 'Upload' do
 
     before(:all) {User.all.map(&:destroy)}
 
-    let(:user) { User.create(name: 'test', email: 'test@abv.bg', password: '123123', role: 'Reseller') }
+    let(:user) {User.create(name: 'test', email: 'test@abv.bg', password: '123123', role: 'Reseller')}
 
 
     it 'has belong to User' do
-      userId  = user.id
-      a = Upload.create(name: 'upload',user_id: userId)
-      expect(a.user_id == userId).to be true
+      USER_ID = user.id
+      a = Upload.create(name: 'upload', user_id: USER_ID)
+      expect(a.user_id == USER_ID).to be true
+    end
+
+    it 'has been validated successfully' do
+      USER_ID = user.id
+      a = Upload.new(name: 'upload', user_id: USER_ID, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/mp4", video_file_size: 1055736)
+      expect(a.save).to be true
+
+    end
+
+    it 'negative test for missing video attributes' do
+      USER_ID = user.id
+      a = Upload.new(name: 'upload', user_id: USER_ID)
+      expect(a.save).to be false
+
+    end
+
+    it 'negative test for name uniqueness' do
+      USER_ID = user.id
+      a = Upload.create(name: 'upload', user_id: USER_ID, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/mp4", video_file_size: 1055736)
+      b = Upload.new(name: 'upload', user_id: USER_ID, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/mp4", video_file_size: 1055736)
+
+      expect(b.save).to be false
+    end
+
+
+    it 'negative test for too short name' do
+      USER_ID = user.id
+      a = Upload.new(name: 'a', user_id: USER_ID)
+      expect(a.save).to be false
+
+    end
+
+    it 'negative test for too large file' do
+      USER_ID = user.id
+      a = Upload.new(name: 'aa', user_id: USER_ID, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/mp4", video_file_size: 105573611111)
+      expect(a.save).to be false
+
+    end
+
+    it 'negative test for unsupported media type' do
+      USER_ID = user.id
+      a = Upload.new(name: 'aa', user_id: USER_ID, video_file_name: "SampleVideo_1280x720_1mb.mp4", video_content_type: "video/avi", video_file_size: 10557)
+      expect(a.save).to be false
 
     end
 
